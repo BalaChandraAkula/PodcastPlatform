50c3682018a7c9da7d555cf3fc5ac2c2ca32f5ce
BalaChandraAkula <balachandraakula698@gmail.com>
2023-09-13 17:21:35 +0530

    L13 uploading file to firebase

--- src/components/StartAPodcast/CreatePodCastForm.js
+++ src/components/StartAPodcast/CreatePodCastForm.js
@@ -5,6 +5,9 @@ import InputComponent from "../common/Input";
 import Button from "../common/Button";
 import { toast } from "react-toastify";
 import FileInput from "../common/Input/FileInput";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { auth, db, storage } from "../../firebase";
+import { addDoc, collection, doc, setDoc } from "firebase/firestore";
 
 function CreatePodCastForm() {
   const [title, setTitle] = useState("");
@@ -16,11 +19,44 @@ function CreatePodCastForm() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const handleSubmit = () => {
-    toast.success("Handling Form");
+  const handleSubmit = async () => {
     if (title && desc && displayImage && bannerImage) {
+      setLoading(true);
+      try {
+        const bannerImageRef = ref(
+          storage,
+          `podcasts/${auth.currentUser.uid}/${Date.now()}`
+        );
+        await uploadBytes(bannerImageRef, bannerImage);
+        const bannerImageUrl = await getDownloadURL(bannerImageRef);
+
+        const displayImageRef = ref(
+          storage,
+          `podcasts/${auth.currentUser.uid}/${Date.now()}`
+        );
+        await uploadBytes(displayImageRef, displayImage);
+        const disaplayImageUrl = await getDownloadURL(displayImageRef);
+
+        const podcastData = {
+          title: title,
+          description: desc,
+          bannerImage: bannerImageUrl,
+          displayImage: disaplayImageUrl,
+          createdBy: auth.currentUser.uid,
+        };
+
+        const docRef = await addDoc(collection(db, "podcasts"), podcastData);
+        toast.success("Created PodCast");
+        setLoading(false);
+      } catch (e) {
+        toast.error(e.message);
+        console.log(e);
+        setLoading(false);
+      }
+      // toast.success("File Uploaded");
     } else {
       toast.error("Please Fill All Values");
+      setLoading(false);
     }
   };
 

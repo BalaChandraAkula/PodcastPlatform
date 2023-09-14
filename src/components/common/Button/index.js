3fe9b0c41ba57dfc96fda7553c95e9cb073ae82a
BalaChandraAkula <balachandraakula698@gmail.com>
2023-09-14 21:59:28 +0530

    L15 Podcast Details Page

--- src/components/common/Button/index.js
+++ src/components/common/Button/index.js
@@ -1,10 +1,17 @@
-import React from 'react' 
-import './styles.css';
+import React from "react";
+import "./styles.css";
 
-function Button({text,onClick,disabled}) {
+function Button({ text, onClick, disabled, style }) {
   return (
-    <div className='custom-btn' disabled={disabled} onClick={onClick}>{text}</div>
-  )
+    <div
+      className="custom-btn"
+      style={style}
+      disabled={disabled}
+      onClick={onClick}
+    >
+      {text}
+    </div>
+  );
 }
 
-export default Button
+export default Button;

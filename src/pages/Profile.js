e3648e042ad6c691ecd23514b9bf49d03dab94c2
BalaChandraAkula <balachandraakula698@gmail.com>
2023-09-22 17:01:31 +0530

    L18 Loading Page Conclusion

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,25 +1,28 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import Header from '../components/common/Header';
-import Button from '../components/common/Button';
-import { signOut } from 'firebase/auth';
-import { auth } from '../firebase';
-import {toast} from "react-toastify"
+import React from "react";
+import { useSelector } from "react-redux";
+import Header from "../components/common/Header";
+import Button from "../components/common/Button";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import { toast } from "react-toastify";
+import Loader from "../components/common/Loader";
 
 function Profile() {
   const user = useSelector((state) => state.user.user);
   console.log(user);
 
-  if(!user){
-    return <p>Loading...</p>
+  if (!user) {
+    return <Loader />;
   }
 
-  const handleLogout =()=>{
-    signOut(auth).then(()=>{
-      toast.success("User Logged OUt!")
-    }).catch((error)=>{
-      toast.error(error.message);
-    });
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        toast.success("User Logged OUt!");
+      })
+      .catch((error) => {
+        toast.error(error.message);
+      });
   };
 
   return (
@@ -28,9 +31,9 @@ function Profile() {
       <h1>{user.name}</h1>
       <h1>{user.email}</h1>
       <h1>{user.uid}</h1>
-      <Button text={"Logout"} onClick={handleLogout}/>
+      <Button text={"Logout"} onClick={handleLogout} />
     </div>
-  )
+  );
 }
 
 export default Profile;

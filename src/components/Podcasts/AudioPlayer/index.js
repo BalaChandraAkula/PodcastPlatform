a2aea0795b8a5af211705b0c9fe8e3430557351e
BalaChandraAkula <balachandraakula698@gmail.com>
2023-09-22 16:40:51 +0530

    L18 Custom Player Logic

--- src/components/Podcasts/AudioPlayer/index.js
+++ src/components/Podcasts/AudioPlayer/index.js
@@ -1,16 +1,18 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./styles.css";
 import { FaPlay, FaPause, FaVolumeUp, FaVolumeMute } from "react-icons/fa";
 
 function AudioPlayer({ audioSrc, image }) {
-  const [duration, setDuration] = useState("");
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
   const [volume, setVolume] = useState(1);
   const [isPlaying, setIsPlaying] = useState(true);
   const [isMute, setIsMute] = useState(false);
   const audioRef = useRef();
 
   const handleDuration = (e) => {
-    setDuration(e.target.value);
+    setCurrentTime(e.target.value);
+    audioRef.current.currentTime = e.target.value;
   };
 
   const togglePlay = () => {
@@ -25,23 +27,87 @@ function AudioPlayer({ audioSrc, image }) {
 
   const handleVolume = (e) => {
     setVolume(e.target.value);
+    audioRef.current.volume = e.target.value;
   };
+
+  const formateTime = (time) => {
+    const minutes = Math.floor(time / 60);
+    const seconds = Math.floor(time % 60);
+    return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+  };
+
+  useEffect(() => {
+    const audio = audioRef.current;
+    audio.addEventListener("timeupdate", handleTimeUpdate);
+    audio.addEventListener("loadedmetadata", handleLoadedMetaData);
+    audio.addEventListener("ended", handleEnded);
+
+    return () => {
+      audio.removeEventListener("timeupdate", handleTimeUpdate);
+      audio.removeEventListener("loadedmetadata", handleLoadedMetaData);
+      audio.removeEventListener("ended", handleEnded);
+    };
+  }, []);
+
+  const handleTimeUpdate = () => {
+    setCurrentTime(audioRef.current.currentTime);
+  };
+
+  const handleLoadedMetaData = () => {
+    setDuration(audioRef.current.duration);
+  };
+
+  const handleEnded = () => {
+    setCurrentTime(0);
+    setIsPlaying(false);
+  };
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+    } else {
+      audioRef.current.pause();
+    }
+  }, [isPlaying]);
+  useEffect(() => {
+    if (!isMute) {
+      audioRef.current.volume = 1;
+      setVolume(1);
+    } else {
+      audioRef.current.volume = 0;
+      setVolume(0);
+    }
+  }, [isMute]);
 
   return (
     <div className="custom-audio-player">
       <img src={image} className="display-image-player" />
       <audio ref={audioRef} src={audioSrc} />
-      <p onClick={togglePlay}>{isPlaying ? <FaPause /> : <FaPlay />}</p>
+      <p className="audio-btn" onClick={togglePlay}>
+        {isPlaying ? <FaPause /> : <FaPlay />}
+      </p>
       <div className="duration-flex">
-        <p>0:00</p>
+        <p>{formateTime(currentTime)}</p>
         <input
           type="range"
+          max={duration}
+          value={currentTime}
           onChange={handleDuration}
           className="duration-range"
         />
-        <p>-21:00</p>
-        <p onClick={toggleMute}>{isMute ? <FaVolumeUp /> : <FaVolumeMute />}</p>
-        <input type="range" onChange={handleVolume} className="volume-range" />
+        <p>{formateTime(duration)}</p>
+        <p className="audio-btn" onClick={toggleMute}>
+          {!isMute ? <FaVolumeUp /> : <FaVolumeMute />}
+        </p>
+        <input
+          type="range"
+          value={volume}
+          max={1}
+          min={0}
+          step={0.01}
+          onChange={handleVolume}
+          className="volume-range"
+        />
       </div>
     </div>
   );
